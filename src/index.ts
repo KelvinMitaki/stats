616610cdd49e41fc6efc995aa0ba5cea91d484d9
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-08 00:28:26 +0300

    replaced string with emun

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,22 @@ const matches = fs
   .readFileSync("football.csv", { encoding: "utf-8" })
   .split("\n")
   .map((row: string): string[] => row.split(","));
-console.log(matches);
+
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D"
+}
+
+let manUWins = 0;
+
+matches.forEach((match: string[]): void => {
+  if (
+    (match[1] === "Man United" && match[5] === MatchResult.HomeWin) ||
+    (match[2] === "Man United" && match[5] === MatchResult.AwayWin)
+  ) {
+    manUWins++;
+  }
+});
+
+console.log(`man u has won ${manUWins} times`);
