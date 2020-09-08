21628de49ebb33d265568a92130fb697e0306aa4
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-08 11:42:59 +0300

    added matchdata tuple

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { CSVFileReader } from "./CSVFileReader";
+import { CSVFileReader, MatchData } from "./CSVFileReader";
 import { MatchResult } from "./matchResult";
 
 const reader = new CSVFileReader("football.csv");
 reader.read();
 let manUWins = 0;
 
-reader.data.forEach((match: string[]): void => {
+reader.data.forEach((match: MatchData): void => {
   if (
     (match[1] === "Man United" && match[5] === MatchResult.HomeWin) ||
     (match[2] === "Man United" && match[5] === MatchResult.AwayWin)
