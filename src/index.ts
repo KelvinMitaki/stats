42bcb4ec4e254af46fb0bacacc75b7c362003a58
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-08 15:29:17 +0300

    added console report ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { CSVFileReader } from "./CSVFileReader";
 import { MatchResult } from "./matchResult";
 import { MatchData, MatchReader } from "./MatchReader";
 import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
 
 const csvFileReader = new CSVFileReader("football.csv");
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
 const teamWins = new WinsAnalysis("Chelsea");
-console.log(teamWins.run(matchReader.matches));
+const consoleReport = new ConsoleReport();
+consoleReport.print(teamWins.run(matchReader.matches));
